a5b38e0566aa7d58aa867eaadbc1e10d1748b8ef
orff1989 <orff1989@walla.com>
2021-11-13 17:54:20 +0200

    hi

---
+++ Building.py
@@ -0,0 +1,7 @@
+class Building:
+
+    def __init__(self, minFloor, maxFloor, elevators) ->None:
+        self._minFloor=minFloor
+        self._maxFloor=maxFloor
+        self._elevators=elevators
+
